4cbeaae22a2831093d91790d04b1949bdefc4aa4
shapik <serega.shapowal@gmail>
2021-05-25 16:38:32 +0300

    1315. Sum of Nodes with Even-Valued Grandparent

---
+++ src/main/java/ru/shapik/leet_code/medium/EvenValuedGrandparent.java
@@ -0,0 +1,45 @@
+package ru.shapik.leet_code.medium;
+
+import ru.shapik.leet_code.data.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EvenValuedGrandparent {
+    public static void main(String[] args) {
+        EvenValuedGrandparent evenValuedGrandparent = new EvenValuedGrandparent();
+        TreeNode treeNode = new TreeNode(6,
+                new TreeNode(7 , new TreeNode(2, new TreeNode(9, null, null),null), new TreeNode(7, new TreeNode(1, null, null), new TreeNode(4, null, null))),
+                new TreeNode(8, new TreeNode(1, null, null), new TreeNode(3, null, new TreeNode(5, null, null))));
+        System.out.println(evenValuedGrandparent.sumEvenGrandparent(treeNode));
+    }
+    public int sumEvenGrandparent(TreeNode root) {
+       Integer num = 0;
+        List<Integer> intList = new ArrayList<>();
+        findNode(root, intList);
+        for (int i = 0; i < intList.size(); i++) {
+            num += intList.get(i);
+        }
+       return num;
+    }
+
+    private void findGrandChildren(TreeNode root, List<Integer> intLis) {
+        if (root != null) {
+            if (root.left != null) intLis.add(root.left.val);
+            if (root.right != null) intLis.add(root.right.val);
+        }
+    }
+
+    private void findNode(TreeNode root, List<Integer> intLis) {
+        if (root != null) {
+            if (root.val%2 == 0) {
+                findGrandChildren(root.left, intLis);
+                findGrandChildren(root.right, intLis);
+            }
+            findNode(root.left, intLis);
+            findNode(root.right, intLis);
+        }
+    }
+}
