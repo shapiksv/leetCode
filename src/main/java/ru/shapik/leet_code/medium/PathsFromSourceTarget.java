89d6ffb5f6385a0827abefb269778f78295eb236
shapiksv <serega.shapowal@gmail.com>
2021-07-10 20:02:57 +0300

    797. All Paths From Source to Target

--- src/main/java/ru/shapik/leet_code/medium/PathsFromSourceTarget.java
+++ src/main/java/ru/shapik/leet_code/medium/PathsFromSourceTarget.java
@@ -15,29 +15,13 @@ public class PathsFromSourceTarget {
     }
 
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        int length = graph.length;
 
-        NodeGraph[] arrayNode = new NodeGraph[length];
-        NodeGraph firstNode;
-        firstNode = new NodeGraph(0, graph[0].length);
-        arrayNode[0] = firstNode;
-
-        for (int i = 1; i < length; i++) {
-            arrayNode[i] = new NodeGraph(i, graph[i].length);
-        }
-
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                NodeGraph nodeGraphs = arrayNode[i];
-                nodeGraphs.arrayNodes[j] = arrayNode[graph[i][j]];
-                System.out.println();
-            }
-        }
         Map<Integer, String> mapGraph = new HashMap<>();
         mapGraph.put(0, "");
-        Integer stop = length - 1;
-        bypass(firstNode, mapGraph, 0, stop);
-        System.out.println(mapGraph.size());
+
+        Integer stop = graph.length - 1;
+        bypass(graph, 0, mapGraph, 0, stop);
+
         List<List<Integer>> request = new ArrayList<>();
         mapGraph.keySet().forEach(kay -> {
             String way = mapGraph.get(kay);
@@ -55,40 +39,28 @@ public class PathsFromSourceTarget {
         return request;
     }
 
-    private void bypass(NodeGraph currentNode, Map<Integer, String> mapGraph, Integer currentKey, Integer stop) {
-        if (mapGraph.get(currentKey).contains(String.valueOf(currentNode.num)) || (!mapGraph.get(currentKey).isBlank() && currentNode.num == 0)) return;
+    private void bypass(int[][] graph, Integer position, Map<Integer, String> mapGraph, Integer currentKey, Integer stop) {
+        if (mapGraph.get(currentKey).contains(String.valueOf(position)) || (!mapGraph.get(currentKey).isBlank() && position == 0)) return;
 
         String value = mapGraph.get(currentKey);
-        if (!mapGraph.get(currentKey).isBlank()) value = value.concat("," + currentNode.num);
-        else value = value.concat("" + currentNode.num);
+        if (!mapGraph.get(currentKey).isBlank()) value = value.concat("," + position);
+        else value = value.concat("" + position);
 
-        if (currentNode.num.equals(stop)) {
+        if (position.equals(stop)) {
             mapGraph.put(currentKey, value);
             return;
         }
 
-        for (int i = 0; i < currentNode.arrayNodes.length; i++) {
+        for (int i = 0; i < graph[position].length; i++) {
             if (i == 0) {
                 mapGraph.put(currentKey, value);
-                bypass(currentNode.arrayNodes[i], mapGraph, currentKey, stop);
+                bypass(graph, graph[position][i], mapGraph, currentKey, stop);
             } else {
                 Integer newKey = mapGraph.size() + 1;
                 mapGraph.put(newKey, value);
-                bypass(currentNode.arrayNodes[i], mapGraph, newKey, stop);
+                bypass(graph, graph[position][i], mapGraph, newKey, stop);
             }
         }
 
     }
-
-    class NodeGraph {
-        Integer num;
-        NodeGraph[] arrayNodes;
-
-        NodeGraph(int num, int size) {
-            this.num = num;
-            this.arrayNodes = new NodeGraph[size];
-        }
-    }
-
-
 }
